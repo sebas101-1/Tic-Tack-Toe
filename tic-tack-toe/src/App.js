eeb35883878af6136ce282ad6075c62572752ebf
sebas101-1 <79105051+sebas101-1@users.noreply.github.com>
2023-03-19 19:36:17 -0700

    did stuff

--- tic-tack-toe/src/App.js
+++ tic-tack-toe/src/App.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 let turn = 1;
 let tempList;
 let width = window.screen.width;
-let margin = (width-360)/2;
+let margin = (width-425)/2;
 let moon = "https://imgs.search.brave.com/Nro8uRsfvKpr25OxdvHc0Gr8xliCpSdglA9w1yjJtpA/rs:fit:980:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfNTk3OC5wbmc";
+let buttonMargin = (width-49)/2;
 let sun = "https://imgs.search.brave.com/6TS-AurMypeRMvUbwkns9XrqGV8dVW450I03cilJqWc/rs:fit:600:600:1/g:ce/aHR0cDovL3d3dy5j/bGlwYXJ0YmVzdC5j/b20vY2xpcGFydHMv/TTlULzRway9NOVQ0/cGthaUUucG5n";
 function Square({ value, clickFunc }) {
   return (
@@ -69,17 +70,20 @@ export default function Board() {
   let [squareValue, Setvalue] = useState(Array(9).fill(" "));
   let [darkMode, setDarkMode] = useState(false);
   let [buttonVal, setButton] = useState(sun);
+  let [borderColor, setborder] = useState("black");
+  let [textColor, settext] = useState("white");
   function buttonClick(){
     setDarkMode(!darkMode);
     if(darkMode){
       setButton(moon);
+      setborder("white")
+      settext("white")
     }
-  }
-  function darkButton({click}){
-    return(
-    <button class="darkMode" onClick={click}>
-      <img alt="dark/light mode"src={buttonVal}/>
-    </button>);
+    else{
+      setButton(sun);
+      setborder("black")
+      settext("white")
+    }
   }
   function calcWinner(boardLisy) {
     let winComb = checkVal(boardLisy);
@@ -123,8 +127,7 @@ export default function Board() {
   return (
     <>
     <div>
-      <p>Tick Tack Toes</p> 
-      <darkButton click={buttonClick}/>
+      <p>Tick Tack Toes</p> <button class="darkMode" onClick={buttonClick} style={{marginLeft: buttonMargin}}> <img alt="dark/light mode"src={buttonVal}/> </button>
     </div>
     <hr />
       <div className="Board" style={{marginLeft: margin}}>
@@ -133,35 +136,39 @@ export default function Board() {
             value={squareValue[0]}
             clickFunc={() => Click(0)}
             className="topLeft"
+            style={{color: textColor, border: borderColor }}
           />
           <Square value={squareValue[1]} clickFunc={() => Click(1)} />
           <Square
             value={squareValue[2]}
             clickFunc={() => Click(2)}
             className="topRight"
+            style={{color: textColor, border: borderColor }}
           />
         </div>
         <div className="board-row">
-          <Square value={squareValue[3]} clickFunc={() => Click(3)} />
-          <Square value={squareValue[4]} clickFunc={() => Click(4)} />
-          <Square value={squareValue[5]} clickFunc={() => Click(5)} />
+          <Square value={squareValue[3]} clickFunc={() => Click(3)} style={{color: textColor, border: borderColor }} />
+          <Square value={squareValue[4]} clickFunc={() => Click(4)} style={{color: textColor, border: borderColor }} />
+          <Square value={squareValue[5]} clickFunc={() => Click(5)} style={{color: textColor, border: borderColor }} />
         </div>
         <div className="board-row">
           <Square
             value={squareValue[6]}
             clickFunc={() => Click(6)}
             className="bottomLeft"
+            style={{color: textColor, border: borderColor }}
           />
-          <Square value={squareValue[7]} clickFunc={() => Click(7)} />
+          <Square value={squareValue[7]} clickFunc={() => Click(7)} style={{color: textColor, border: borderColor }} />
           <Square
             value={squareValue[8]}
             clickFunc={() => Click(8)}
             className="bottomRight"
+            style={{color: textColor, border: borderColor }}
           />
         </div>
       </div>
       <p class="text">{squareValue}</p>
-      <p class="text">hi {winner}</p>
+      <p class="text">{winner}</p>
       <Reset Clicked={resetClick} /> 
     </>
   );
