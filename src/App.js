4d640895a1b68c7946e67e92c37da42ff3bb87da
sebas101-1 <79105051+sebas101-1@users.noreply.github.com>
2023-03-21 21:12:29 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ let tempList;
 let width = window.screen.width;
 let margin = (width-425)/2;
 let moon = "https://imgs.search.brave.com/Nro8uRsfvKpr25OxdvHc0Gr8xliCpSdglA9w1yjJtpA/rs:fit:980:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfNTk3OC5wbmc";
-let buttonMargin = (width-49)/2;
+let buttonMargin = (width-70)/2;
 let sun = "https://imgs.search.brave.com/HFgqxgJ-iXg4pKp2qy9w5Gf21EvVq-5gRaUMlkGTJpk/rs:fit:512:512:1/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvc2ltcGxpY2l0/eS12ZWN0b3ItaWNv/bi1zZXQvNTEyL3N1/bi5wbmc";
 function Square({ value, clickFunc }) {
   return (
@@ -70,19 +70,16 @@ export default function Board() {
   let [squareValue, Setvalue] = useState(Array(9).fill(" "));
   let [darkMode, setDarkMode] = useState(false);
   let [buttonVal, setButton] = useState(sun);
-  let [borderColor, setborder] = useState("black");
-  let [textColor, settext] = useState("white");
+  let[squareClass, setClass] = useState("light ")
   function buttonClick(){
     setDarkMode(!darkMode);
     if(darkMode){
       setButton(moon);
-      setborder("white")
-      settext("white")
+      setClass("dark ")
     }
     else{
       setButton(sun);
-      setborder("black")
-      settext("white")
+      setClass("light ")
     }
   }
   function calcWinner(boardLisy) {
@@ -135,35 +132,34 @@ export default function Board() {
           <Square
             value={squareValue[0]}
             clickFunc={() => Click(0)}
-            className="topLeft"
-            style={{color: textColor, border: borderColor }}
+            className={{squareClass}+"topLeft"}
           />
           <Square value={squareValue[1]} clickFunc={() => Click(1)} />
           <Square
             value={squareValue[2]}
             clickFunc={() => Click(2)}
-            className="topRight"
-            style={{color: textColor, border: borderColor }}
+            className={squareClass+"topRight"}
+            
           />
         </div>
         <div className="board-row">
-          <Square value={squareValue[3]} clickFunc={() => Click(3)} style={{color: textColor, border: borderColor }} />
-          <Square value={squareValue[4]} clickFunc={() => Click(4)} style={{color: textColor, border: borderColor }} />
-          <Square value={squareValue[5]} clickFunc={() => Click(5)} style={{color: textColor, border: borderColor }} />
+          <Square value={squareValue[3]} className={squareClass} clickFunc={() => Click(3)}  />
+          <Square value={squareValue[4]} className={squareClass} clickFunc={() => Click(4)}  />
+          <Square value={squareValue[5]} className={squareClass} clickFunc={() => Click(5)}  />
         </div>
         <div className="board-row">
           <Square
             value={squareValue[6]}
             clickFunc={() => Click(6)}
-            className="bottomLeft"
-            style={{color: textColor, border: borderColor }}
+            className={squareClass+"bottomLeft"}
+            
           />
-          <Square value={squareValue[7]} clickFunc={() => Click(7)} style={{color: textColor, border: borderColor }} />
+          <Square value={squareValue[7]} clickFunc={() => Click(7)} textColor  />
           <Square
             value={squareValue[8]}
             clickFunc={() => Click(8)}
-            className="bottomRight"
-            style={{color: textColor, border: borderColor }}
+            className={squareClass+"bottomRight"}
+            
           />
         </div>
       </div>
